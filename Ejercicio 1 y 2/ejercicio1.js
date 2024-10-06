a4baf2be59e3bb1255bfb5f7e0e333dfabb91347
jumice <jmcoen93@gmail.com>
2024-10-06 12:44:57 +0200

    Hemos introducido h2 para que cambie segun el mouseover

--- Ejercicio 1 y 2/ejercicio1.js
+++ Ejercicio 1 y 2/ejercicio1.js
@@ -1,5 +1,8 @@
 let categorias = document.getElementsByClassName('style_wrapper');
+
 let NombreCategoriasFrutas = ["melon", "orange", "peach", "strawberry", "watermelon"];
+let NombreCategoriasVerduras = ["Cebolla_Dulce", "Cebolla_morada", "Tomate_cherry", "Tomate_raf", "Tomate_rosa"];
+
 let ContenidoFrutas = [
     {categoria: 'melon', rutas: "images/frutas/melon/1.jpg"},
     {categoria: 'orange', rutas: "images/frutas/orange/1.jpg"},
@@ -8,8 +11,6 @@ let ContenidoFrutas = [
     {categoria: 'watermelon', rutas: "images/frutas/watermelon/1.jpg"}
 ];
 
-
-let NombreCategoriasVerduras = ["Cebolla_Dulce", "Cebolla_morada", "Tomate_cherry", "Tomate_raf", "Tomate_rosa"];
 let ContenidoVerduras = [
     {categoria: 'Cebolla_Dulce', rutas: "images/verduras/cebolla_dulce/1.jpg"},
     {categoria: 'Cebolla_morada', rutas: "images/verduras/cebolla_morada/1.jpg"},
@@ -18,29 +19,42 @@ let ContenidoVerduras = [
     {categoria: 'Tomate_rosa', rutas: "images/verduras/tomate_rosa/1.jpg"}
 ];
 
+h1 = document.querySelector('h1');
+let h2 = document.querySelector('h2');
+let PreviousButton = document.getElementsByTagName('button')[0];
+let NextButton = document.getElementsByTagName('button')[1];
 
-h1 = document.getElementsByTagName('h1');
-
-function CategoriaFrutas(){
-for (let i = 0; i < categorias.length; i++) {
-    let Categoria = categorias[i].querySelector('p'); 
-    Categoria.textContent = NombreCategoriasFrutas[i];
-
-    categorias[i].addEventListener('mouseover', function() {
-        Categoria.style.backgroundImage = `url(${ContenidoFrutas[i].rutas})`;
-        Categoria.style.backgroundSize = 'cover';
-    });
-
-    categorias[i].addEventListener('mouseout', function() {
-        Categoria.style.backgroundImage = ''; 
-    });
+function clearEventListeners() {
+    for (let i = 0; i < categorias.length; i++) {
+        let clone = categorias[i].cloneNode(true);
+        categorias[i].parentNode.replaceChild(clone, categorias[i]);
+    }
 }
 
-h1.textContent= 'Frutas';
+// Funcion que llama el formulario para cambiar a categoria frutas
+function CategoriaFrutas() {
+    clearEventListeners(); 
+    for (let i = 0; i < categorias.length; i++) {
+        let Categoria = categorias[i].querySelector('p'); 
+        Categoria.textContent = NombreCategoriasFrutas[i];
+
+        categorias[i].addEventListener('mouseover', function() {
+            Categoria.style.backgroundImage = `url(${ContenidoFrutas[i].rutas})`;
+            Categoria.style.backgroundSize = 'cover';
+            h2.textContent = ContenidoFrutas[i].categoria;
+        });
+
+        categorias[i].addEventListener('mouseout', function() {
+            Categoria.style.backgroundImage = ''; 
+        });
+    }
+
+    h1.textContent = 'Frutas';
 }
 
-
-function CategoriaVerduras(){
+// Funcion que llama el formulario para cambiar a categoria verduras
+function CategoriaVerduras() {
+    clearEventListeners(); 
     for (let i = 0; i < categorias.length; i++) {
         let Categoria = categorias[i].querySelector('p'); 
         Categoria.textContent = NombreCategoriasVerduras[i];
@@ -48,6 +62,7 @@ function CategoriaVerduras(){
         categorias[i].addEventListener('mouseover', function() {
             Categoria.style.backgroundImage = `url(${ContenidoVerduras[i].rutas})`;
             Categoria.style.backgroundSize = 'cover';
+            h2.textContent = ContenidoVerduras[i].categoria;
         });
     
         categorias[i].addEventListener('mouseout', function() {
@@ -55,8 +70,8 @@ function CategoriaVerduras(){
         });
     }
 
-    h1.textContent= 'Verduras';
-    } 
+    h1.textContent = 'Verduras';
+}
 
-
-    CategoriaFrutas()
+// Para que arranque con categoria frutas
+CategoriaFrutas();
